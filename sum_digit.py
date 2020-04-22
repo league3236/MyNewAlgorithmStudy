ec0843632769b0cca2711fadd5283190cc2b0d7c
sh_kwak <sh_kwak@lotte.net>
2020-04-22 22:10:20 +0900

    [codeit]/sum_digit/200422

---
+++ sum_digit.py
@@ -0,0 +1,13 @@
+def sum_digits(n):
+    if n<10:
+        return n
+    return  n%10+sum_digits(n/10)
+
+print(22541%10)
+print((22541/10))
+
+print(sum_digits(22541))
+print(sum_digits(92130))
+print(sum_digits(12634))
+print(sum_digits(704))
+print(sum_digits(3755))
