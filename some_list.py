439b65491a677a661206eeaa694dd36435cf1540
sh_kwak <sh_kwak@lotte.net>
2020-04-22 23:02:45 +0900

    [codeit]/backlist/200422

---
+++ some_list.py
@@ -0,0 +1,12 @@
+
+def flip(some_list):
+    if len(some_list) == 0 or len(some_list) == 1:
+        return some_list
+    else:
+        return some_list[-1:]+flip(some_list[:-1])
+
+
+some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(some_list[-1:])
+some_list = flip(some_list)
+print(some_list)
