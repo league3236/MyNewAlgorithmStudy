72eab5d5a4135d06831ba4d3014fbd3cb0d5f8c5
league3236 <league3236@gmail.com>
2020-04-24 22:44:22 +0900

    [codeit]/trapping_rain/200424

---
+++ trapping_rain.py
@@ -0,0 +1,10 @@
+def trapping_rain(buildings):
+    # 코드를 작성하세요
+    trapped_rain = 0
+    current_tallest = 0
+    total_tallest = max(buildings)
+        
+
+# 테스트
+print(trapping_rain([3, 0, 0, 2, 0, 4]))
+print(trapping_rain([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
