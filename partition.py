aeeb6aa90cc76688c7d0a06eb27e21406b22aab5
league3236 <league3236@gmail.com>
2020-04-26 20:35:59 +0900

    [codeit]/partitioning2/200425

--- partition.py
+++ partition.py
@@ -25,14 +25,31 @@ def partition(my_list, start, end):
 
     return p
 
+    
+# 퀵 정렬
+def quicksort(my_list, start, end):
+    # 코드를 작성하세요.
+    if end - start < 1:
+        return
+    
+    pivot = partition(my_list, start, end)
+
+    quicksort(my_list, start, pivot - 1)
+
+    quicksort(my_list, pivot + 1, end)
+
+
 # 테스트 1
-list1 = [4, 3, 6, 2, 7, 1, 5]
-pivot_index1 = partition(list1, 0, len(list1) - 1)
+list1 = [1, 3, 5, 7, 9, 11, 13, 11]
+quicksort(list1, 0, len(list1) - 1)
 print(list1)
-print(pivot_index1)
 
 # 테스트 2
-list2 = [6, 1, 2, 6, 3, 5, 4]
-pivot_index2 = partition(list2, 0, len(list2) - 1)
+list2 = [28, 13, 9, 30, 1, 48, 5, 7, 15]
+quicksort(list2, 0, len(list2) - 1)
 print(list2)
-print(pivot_index2)
+
+# 테스트 3
+list3 = [2, 5, 6, 7, 1, 2, 4, 7, 10, 11, 4, 15, 13, 1, 6, 4]
+quicksort(list3, 0, len(list3) - 1)
+print(list3)
