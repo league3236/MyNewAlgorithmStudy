242752fe9f7dab378f8d546e285e7b9bd91f554c
league3236 <league3236@gmail.com>
2020-07-15 17:36:19 +0900

    [programmers]/2016year/200715

---
+++ 2016year.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*- 
+# 2016년 1월 1일은 금요일
+# 2016년 a월 b일은 무슨요일?
+# 두수 a, b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수를 작성
+# 2016년은 윤년임
+# 2016년 a월 b일은 실제 있는 날이다.
+
+def solution(a, b):
+    answer = ''
+    sum = 0
+    enum = ['FRI','SAT','SUN','MON','TUE','WED','THU']
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    # for idx, value in enumerate(month):
+    #     print(value)
+
+    for idx in range(0,a-1):
+        print(idx)
+
+    return answer
+
+solution(5,24)
