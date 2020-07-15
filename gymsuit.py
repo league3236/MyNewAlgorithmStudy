6b98bbd2de63e018545bf65b367a814f70b4969c
league3236 <league3236@gmail.com>
2020-07-15 09:28:31 +0900

    [programmer]/gymsuit/200715

--- gymsuit.py
+++ gymsuit.py
@@ -1,3 +1,4 @@
+# 프로그래머스 체육복 알고리즘 1일차
 # 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있음
 # 최대한 많은 학생이 체육수업을 들어야 함
 # 전체 학생의 수 n
@@ -5,42 +6,26 @@
 # 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수
 # 체육수업을 들을 수 있는 학생의 최댓값을 return
 
-def people_list(n, lost, reserve):
-    people = [0]*n
-    for i in range(1,n+1):
-        if i in lost:
-            people[i-1] = 0
-            continue
-        if i in reserve:
-            people[i-1] = 2
-        else:
-            people[i-1] = 1
-    return people
 
 def solution(n, lost, reserve):
     answer = 0
-    people = people_list(n, lost, reserve)
-    #people = [2, 0, 0, 2]
-    for i in range(0, n):
-        if people[i] == 0:
-            if i == 0:
-                if people[i+1] == 2:
-                    people[i+1]-=1
-                    people[i]+=1
-            elif i == n-1:
-                if people[i-1] == 2:
-                    people[i-1]-=1
-                    people[i]+=1
-            else:
-                if people[i-1] == 2:
-                    people[i-1]-=1
-                    people[i]+=1
-                elif people[i+1] == 2:
-                    people[i+1]-=1
-                    people[i]+=1
-    for i in range(0,n):
-        if people[i]==1:
-            answer+=1
+
+    cloth = [1] * n
+
+    for idx in lost:
+        cloth[idx - 1] -= 1
+    
+    for idx in reserve:
+        cloth[idx - 1] += 1
+
+    for idx, value in enumerate(cloth):
+        if idx>0 and value == 0 and cloth[idx-1] == 2:
+            cloth[idx-1] = 1
+            cloth[idx] = 1 
+        elif idx <n-1 and value ==0 and cloth[idx+1] == 2:
+            cloth[idx+1] = 1
+            cloth[idx] = 1
+    answer = n-cloth.count(0)
     return answer
 
 if __name__ == "__main__":
@@ -48,3 +33,4 @@ if __name__ == "__main__":
     print(solution(5, [2,4], [1,3,5]))
     print(solution(5, [2,4], [3]))
     print(solution(3, [3], [1]))
+
