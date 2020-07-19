31c6bdbeed2267649abaf83704dcea92902c1d62
league3236 <league3236@gmail.com>
2020-07-19 23:30:39 +0900

    [programmers]/가운데글자/200719

--- midchar.py
+++ midchar.py
@@ -4,7 +4,17 @@
 
 def solution(s):
     answer = ''
+    len_s = len(s)
+
+    if len_s <= 2:
+        return s
+
+    mid = int(len_s/2)
+    if len_s%2 == 0: # 짝수
+        answer = s[mid-1]+s[mid]
+    else: # 홀수
+        answer = s[mid]
     return answer
 
 
-solution("abcde")
+print(solution("abc"))
