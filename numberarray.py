361105530d54307eb522a94ca03f545dab580016
league3236 <league3236@gmail.com>
2020-08-05 18:13:58 +0900

    [programmers]/numberarray/200805

--- numberarray.py
+++ numberarray.py
@@ -1,12 +1,17 @@
 # array의 각 element중 divisor로 나누어 떨어지는 값을 오른차순으로 정렬한 배열로 반환
 
 
+# def solution(arr, divisor):
+#     answer = [i for i in arr if i%divisor==0]
+#     if not answer:
+#         return [-1]
+#     answer.sort()
+#     return answer
+
 def solution(arr, divisor):
-    answer = [i for i in arr if i%divisor==0]
-    if not answer:
-        return [-1]
-    answer.sort()
-    return answer
+    return sorted([i for i in arr if i%divisor==0]) or [-1]
+
+
 
 list = solution([2,36,1,3],5)
 print(list)
