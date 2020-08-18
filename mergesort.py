51d5baa219f0b7ac54756a847e85b59e9151ad8b
league3236 <league3236@gmail.com>
2020-08-18 17:19:59 +0900

    [algo]/mergesort/200818

--- mergesort.py
+++ mergesort.py
@@ -1,19 +1,31 @@
 # 병합정렬
 
-def merge():
-    answer = []
-    return answer
+def merge(left, right):
+    result = []
+    while len(left) > 0 or len(right) > 0 :
+        if len(left) > 0 and len(right) > 0:
+            if left[0] <= right[0]:
+                result.append(left[0])
+                left = left[1:]
+            else:
+                result.append(right[0])
+                right = right[1:]
+        elif len(left) > 0:
+            result.append(left[0])
+            left = left[1:]
+        elif len(right) > 0:
+            result.append(right[0])
+            right = right[1:]
+        # print(result)
+    return result
 
-def merge_sort(list, l, r):
-    if l < r :
-        m = l + (r-1) / 2
-        print(merge_sort(list, m+1, r))  
-    
-    # merge_sort(list, 0, m-1)
-    # merge_sort(list, m,)
-    return list
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+    m = len(list) // 2
+    return merge(merge_sort(list[:m]),merge_sort(list[m:]))
 
 
 list1 = [3,2,1,5]
-result = merge_sort([3,2,1,5], 0, len(list1))
+result = merge_sort([3,2,1,5])
 print(result)
